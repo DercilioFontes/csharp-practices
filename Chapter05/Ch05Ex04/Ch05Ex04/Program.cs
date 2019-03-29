699db92b211047940585a14f3708720f8755ba75
DercilioFontes <dercilioafontes@gmail.com>
2019-03-29 12:37:16 -0700

    Added Ch05Ex04

---
+++ Chapter05/Ch05Ex04/Ch05Ex04/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Ch05Ex04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] friendNames = { "Todd Anthony", "Kevin Holton", "Shane Laigle" };
+
+            WriteLine($"Here are {friendNames.Length} of my friends:");
+
+            for (int i = 0; i < friendNames.Length; i++)
+            {
+                WriteLine(friendNames[i]);
+            }
+
+            ReadKey();
+        }
+    }
+}
