717bf3bb0f2401ab9181f653afa3a3756c019501
DercilioFontes <dercilioafontes@gmail.com>
2019-05-14 10:50:28 -0700

    Finished Ch10CardLib and Added Ch10ClassClient

---
+++ Chapter10/Ch10CardLib/Ch10CardLib/Card.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ch10CardLib
+{
+    public class Card
+    {
+        private readonly Rank rank;
+        private readonly Suit suit;
+
+        private Card()
+        {
+        }
+
+        public Card(Suit newSuit, Rank newRank)
+        {
+            suit = newSuit;
+            rank = newRank;
+        }
+
+        public override string ToString()
+        {
+            return "The " + rank + " of " + suit + "s";
+        }
+    }
+}
