f332b9c709ea8740c762a4a0d14afdf26f2f2d38
DercilioFontes <derciliofontes@gmail.com>
2019-05-18 16:19:01 -0700

    Added SimpleIteratos

---
+++ Chapter11/SimpleIteratos/SimpleIteratos/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+
+namespace SimpleIteratos
+{
+    class MainClass
+    {
+        public static IEnumerable SimpleList()
+        {
+            yield return "string 1";
+            yield return "string 2";
+            yield return "string 3";
+        }
+
+        public static void Main(string[] args)
+        {
+            foreach(string item in SimpleList())
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
