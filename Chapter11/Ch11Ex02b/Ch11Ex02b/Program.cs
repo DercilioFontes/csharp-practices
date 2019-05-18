a3e34b5999db5321ef7592279ff04e46eba84cd9
DercilioFontes <derciliofontes@gmail.com>
2019-05-17 22:50:48 -0700

    Added Program.cs

---
+++ Chapter11/Ch11Ex02b/Ch11Ex02b/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Ch11Ex02b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animals animalCollection = new Animals();
+            animalCollection.Add("Donna", (Animal)(new Cow("Donna")));
+            animalCollection.Add("Kevin", (Animal)(new Chicken("Kevin")));
+            foreach(DictionaryEntry myEntry in animalCollection)
+            {
+                ((Animal)myEntry.Value).Feed();
+                WriteLine(((Animal)myEntry.Value).Name);
+            }
+            ReadKey();
+        }
+    }
+}
