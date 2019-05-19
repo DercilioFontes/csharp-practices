9e17f9c2d9bab96024ecf7cf9c9a2466fb559fe1
DercilioFontes <derciliofontes@gmail.com>
2019-05-18 17:12:35 -0700

    Added Ch11Ex03

---
+++ Chapter11/Ch11Ex03/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using static System.Console;
+
+namespace Ch11Ex03
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Primes primesFrom2To1000 = new Primes(2, 10000000);
+            foreach (long i in primesFrom2To1000)
+                Write($"{i} ");
+            ReadKey();
+        }
+    }
+}
