3f533cac21a5f4b4e5422d6e4a5bf624e3f72dac
DercilioFontes <derciliofontes@gmail.com>
2019-05-18 23:43:33 -0700

    Added DeepClone

---
+++ Chapter11/DeepClone/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using static System.Console;
+
+namespace DeepClone
+{
+    public class Content
+    {
+        public int Val;
+    }
+
+    public class Cloner : ICloneable
+    {
+        public Content MyContent = new Content();
+        public Cloner(int newVal)
+        {
+            MyContent.Val = newVal;
+        }
+        public object Clone()
+        {
+            Cloner clonedCloner = new Cloner(MyContent.Val);
+            return clonedCloner;
+        }
+    }
+
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Cloner mySource = new Cloner(5);
+            Cloner myTarget = (Cloner)mySource.Clone();
+            WriteLine($"myTarget.MyContent.Val = {myTarget.MyContent.Val}");
+            mySource.MyContent.Val = 2;
+            WriteLine($"myTarget.MyContent.Val = {myTarget.MyContent.Val}");
+        }
+    }
+}
