ea0033fbff3e1c1ba3a8777e547e6f4f8556b5f3
DercilioFontes <derciliofontes@gmail.com>
2019-05-22 10:14:01 -0700

    Added Ch12Ex01

---
+++ Chapter12/Ch12Ex01/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using static System.Console;
+
+namespace Ch12Ex01
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Vector v1 = GetVector("vector1");
+            Vector v2 = GetVector("vector2");
+            WriteLine($"{v1} + {v2} = {v1 + v2}");
+            WriteLine($"{v1} - {v2} = {v1 - v2}");
+            ReadKey();
+        }
+        static Vector GetVector(string name)
+        {
+            WriteLine($"Input {name} magniture:");
+            double? r = GetNullableDouble();
+            WriteLine($"Input {name} angle (in degrees):");
+            double? theta = GetNullableDouble();
+            return new Vector(r, theta);
+        }
+        static double? GetNullableDouble()
+        {
+            double? result;
+            string userInput = ReadLine();
+            try
+            {
+                result = double.Parse(userInput);
+            }
+            catch
+            {
+                result = null;
+            }
+            return result;
+        }
+    }
+}
