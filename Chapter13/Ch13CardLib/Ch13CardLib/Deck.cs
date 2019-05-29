65ff582fad0fbcd52245e231a6d750a0d9297c33
DercilioFontes <dercilioafontes@gmail.com>
2019-05-29 10:21:15 -0700

    Added Ch13CardLib and Ch13CardClient

---
+++ Chapter13/Ch13CardLib/Ch13CardLib/Deck.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ch13CardLib
+{
+    public class Deck : ICloneable
+    {
+        private Cards cards = new Cards();
+
+        public Deck()
+        {
+            for(int suitVal = 0; suitVal < 4; suitVal++)
+            {
+                for(int rankVal = 1; rankVal < 14; rankVal++)
+                {
+                    cards.Add(new Card((Suit)suitVal, (Rank)rankVal));
+                }
+            }
+        }
+
+        /// <summary>
+        /// Nondefault constructor. Allows aces to be set high.
+        /// </summary>
+        /// <param name="isAceHigh">If set to <c>true</c> is ace high.</param>
+        public Deck(bool isAceHigh) : this()
+        {
+            Card.isAceHigh = isAceHigh;
+        }
+
+        /// <summary>
+        /// Nondefault constructor. Allows a trump suit to be used.
+        /// </summary>
+        /// <param name="useTrumps">If set to <c>true</c> use trumps.</param>
+        /// <param name="trump">Trump.</param>
+        public Deck(bool useTrumps, Suit trump) : this()
+        {
+            Card.useTrumps = useTrumps;
+            Card.trump = trump;
+        }
+
+        /// <summary>
+        /// Nondefault constructor. Allows aces to be set high and a trump suit
+        /// to be used.
+        /// </summary>
+        /// <param name="isAceHigh">If set to <c>true</c> is ace high.</param>
+        /// <param name="useTrumps">If set to <c>true</c> use trumps.</param>
+        /// <param name="trump">Trump.</param>
+        public Deck(bool isAceHigh, bool useTrumps, Suit trump) : this()
+        {
+            Card.isAceHigh = isAceHigh;
+            Card.useTrumps = useTrumps;
+            Card.trump = trump;
+        }
+
+        public Card GetCard(int cardNum)
+        {
+            if (cardNum >= 0 && cardNum <= 51)
+                return cards[cardNum];
+            else
+                throw new CardOutOfRangeException(cards.Clone() as Cards);
+        }
+
+        public void Shuffle()
+        {
+            Cards newDeck = new Cards();
+            bool[] assigned = new bool[52];
+            Random sourceGen = new Random();
+            for(int i = 0; i < 52; i++)
+            {
+                int sourceCard = 0;
+                bool foundCard = false;
+                while(foundCard == false)
+                {
+                    sourceCard = sourceGen.Next(52);
+                    if (assigned[sourceCard] == false)
+                        foundCard = true;
+                }
+                assigned[sourceCard] = true;
+                newDeck.Add(cards[sourceCard]);
+            }
+            newDeck.CopyTo(cards);
+        }
+
+        private Deck(Cards newCards)
+        {
+            cards = newCards;
+        }
+
+        public object Clone()
+        {
+            Deck newDeck = new Deck(cards.Clone() as Cards);
+            return newDeck;
+        }
+    }
+}
