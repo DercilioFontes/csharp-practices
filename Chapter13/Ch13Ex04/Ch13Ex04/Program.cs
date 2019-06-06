51ca5b3f811e2b9105b0f891e1e7e822ef98fbc9
DercilioFontes <dercilioafontes@gmail.com>
2019-06-06 11:11:51 -0700

    Added Ch13Ex04

---
+++ Chapter13/Ch13Ex04/Ch13Ex04/Program.cs
@@ -0,0 +1,20 @@
+﻿using static System.Console;
+
+namespace Ch13Ex04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Farm<Animal> farm = new Farm<Animal>
+            {
+                new Cow {Name="Lea" },
+                new Chicken { Name="Noa" },
+                new Chicken(),
+                new SuperCow {Name="Andrea" }
+            };
+            farm.MakeNoises();
+            ReadKey();
+        }
+    }
+}
