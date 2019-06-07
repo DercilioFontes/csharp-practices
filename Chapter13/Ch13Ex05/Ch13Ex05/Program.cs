0a98bfb70c70594203c76df445fff626be75fef8
DercilioFontes <dercilioafontes@gmail.com>
2019-06-07 10:26:57 -0700

    Added initial Ch13Ex05

---
+++ Chapter13/Ch13Ex05/Ch13Ex05/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch13Ex05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var curries = new[]
+            {
+                new {MainIngredient = "Lamb", Style = "Dhansak", Spiciness = 5},
+                new {MainIngredient = "Lamb", Style = "Dhansak", Spiciness = 5},
+                new {MainIngredient = "Chicken", Style = "Dhansak", Spiciness = 5},
+            };
+
+        }
+    }
+}
