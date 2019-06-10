da19995277fdc774dc324df29b22ebf8eb25b601
DercilioFontes <dercilioafontes@gmail.com>
2019-06-10 10:36:42 -0700

    Finished the Ch13Ex05

--- Chapter13/Ch13Ex05/Ch13Ex05/Program.cs
+++ Chapter13/Ch13Ex05/Ch13Ex05/Program.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using static System.Console;
 
 namespace Ch13Ex05
 {
@@ -16,7 +12,15 @@ namespace Ch13Ex05
                 new {MainIngredient = "Lamb", Style = "Dhansak", Spiciness = 5},
                 new {MainIngredient = "Chicken", Style = "Dhansak", Spiciness = 5},
             };
-
+            WriteLine(curries[0].ToString());
+            WriteLine(curries[0].GetHashCode());
+            WriteLine(curries[1].GetHashCode());
+            WriteLine(curries[2].GetHashCode());
+            WriteLine(curries[0].Equals(curries[1]));
+            WriteLine(curries[0].Equals(curries[2]));
+            WriteLine(curries[0] == curries[1]);
+            WriteLine(curries[0] == curries[2]);
+            ReadKey();
         }
     }
 }
