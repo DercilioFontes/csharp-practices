0f6f7799a7cc7216bcf3ce83574ba6691835c954
DercilioFontes <derciliofontes@gmail.com>
2019-08-10 19:34:43 -0700

    Added Ch16Ex01

---
+++ Chapter16/Ch16Ex01/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.IO;
+using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using static System.Console;
+
+namespace Ch16Ex01
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                // creates the container
+                CloudStorageAccount storageAccount = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("StorageConnectionString"));
+                CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+                CloudBlobContainer container = blobClient.GetContainerReference("carddeck");
+
+                if (container.CreateIfNotExists())
+                {
+                    WriteLine($"Created container '{container.Name}' in storage account '{storageAccount.Credentials.AccountName}'.");
+                }
+                else
+                {
+                    WriteLine($"Container '{container.Name}' alreary exists for storage account '{storageAccount.Credentials.AccountName}'.");
+                }
+
+                container.SetPermissions(new BlobContainerPermissions { PublicAccess = BlobContainerPublicAccessType.Blob });
+                WriteLine($"Permission for container '{container.Name}' is public.");
+
+                // uploads the card images
+                int numberOfCards = 0;
+                DirectoryInfo dir = new DirectoryInfo(@"Cards");
+                foreach(FileInfo f in dir.GetFiles("*.*"))
+                {
+                    CloudBlockBlob blockBlob = container.GetBlockBlobReference(f.Name);
+                    using(var fileStream = File.OpenRead(@"Cards/" + f.Name))
+                    {
+                        blockBlob.UploadFromStream(fileStream);
+                        WriteLine($"Uploading: '{f.Name}' which is {fileStream.Length} bytes.");
+
+                    }
+                    numberOfCards++;
+                }
+                WriteLine($"Uploaded {numberOfCards.ToString()} cards.");
+                WriteLine();
+
+
+                // to check that all went well
+                numberOfCards = 0;
+                foreach(IListBlobItem item in container.ListBlobs(null, false))
+                {
+                    if(item.GetType() == typeof(CloudBlockBlob))
+                    {
+                        CloudBlockBlob blob = (CloudBlockBlob)item;
+                        WriteLine($"Card image url '{blob.Uri}' with length of {blob.Properties.Length}.");
+                    }
+                    numberOfCards++;
+                }
+                WriteLine($"Listed {numberOfCards.ToString()} cards.");
+
+                // if desired, deletes the images
+                WriteLine("Enter Y to delete listed cards, press enter to skip deletion:");
+                if(ReadLine() == "Y")
+                {
+                    numberOfCards = 0;
+                    foreach (IListBlobItem item in container.ListBlobs(null, false))
+                    {
+                        CloudBlockBlob blob = (CloudBlockBlob)item;
+                        CloudBlockBlob blockBlobToDelete = container.GetBlockBlobReference(blob.Name);
+                        blockBlobToDelete.Delete();
+                        WriteLine($"Deleted: '{blob.Name}' which was {blob.Properties.Length} bytes.");
+                        numberOfCards++;
+                    }
+                    WriteLine($"Deleted {numberOfCards.ToString()} cards.");
+                }
+            }
+            catch (StorageException ex)
+            {
+                WriteLine($"StorageException: {ex.Message}");
+            }
+            catch(Exception ex)
+            {
+                WriteLine($"Exception: {ex.Message}");
+            }
+            WriteLine("Press enter to exit.");
+            ReadLine();
+        }
+    }
+}
