b888d617c3985ac6fe08f582a725a6ca1018e071
DercilioFontes <dercilioafontes@gmail.com>
2019-08-13 10:40:13 -0700

    Implemented new stuff in the CardLib

--- Chapter13/Ch13CardLib/Ch13CardLib/Deck.cs
+++ Chapter13/Ch13CardLib/Ch13CardLib/Deck.cs
@@ -1,67 +1,74 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace Ch13CardLib
 {
+    public delegate void LastCardDrawnHandler(Deck currentDeck);
     public class Deck : ICloneable
     {
-        public event EventHandler LastCardDrawn;
+        public event LastCardDrawnHandler LastCardDrawn;
 
         private Cards cards = new Cards();
 
         public Deck()
         {
-            for (int suitVal = 0; suitVal < 4; suitVal++)
-            {
-                for (int rankVal = 1; rankVal < 14; rankVal++)
-                {
-                    cards.Add(new Card((Suit)suitVal, (Rank)rankVal));
-                }
-            }
+            InsertAllCards();
+            //for (int suitVal = 0; suitVal < 4; suitVal++)
+            //{
+            //    for (int rankVal = 1; rankVal < 14; rankVal++)
+            //    {
+            //        cards.Add(new Card((Suit)suitVal, (Rank)rankVal));
+            //    }
+            //}
+        }
+        protected Deck(Cards newCards)
+        {
+            cards = newCards;
+        }
+        public int CardsInDeck
+        {
+            get { return cards.Count; }
         }
 
-        /// <summary>
-        /// Nondefault constructor. Allows aces to be set high.
-        /// </summary>
-        /// <param name="isAceHigh">If set to <c>true</c> is ace high.</param>
-        public Deck(bool isAceHigh) : this()
-        {
-            Card.isAceHigh = isAceHigh;
-        }
+        ///// <summary>
+        ///// Nondefault constructor. Allows aces to be set high.
+        ///// </summary>
+        ///// <param name="isAceHigh">If set to <c>true</c> is ace high.</param>
+        //public Deck(bool isAceHigh) : this()
+        //{
+        //    Card.isAceHigh = isAceHigh;
+        //}
 
-        /// <summary>
-        /// Nondefault constructor. Allows a trump suit to be used.
-        /// </summary>
-        /// <param name="useTrumps">If set to <c>true</c> use trumps.</param>
-        /// <param name="trump">Trump.</param>
-        public Deck(bool useTrumps, Suit trump) : this()
-        {
-            Card.useTrumps = useTrumps;
-            Card.trump = trump;
-        }
+        ///// <summary>
+        ///// Nondefault constructor. Allows a trump suit to be used.
+        ///// </summary>
+        ///// <param name="useTrumps">If set to <c>true</c> use trumps.</param>
+        ///// <param name="trump">Trump.</param>
+        //public Deck(bool useTrumps, Suit trump) : this()
+        //{
+        //    Card.useTrumps = useTrumps;
+        //    Card.trump = trump;
+        //}
 
-        /// <summary>
-        /// Nondefault constructor. Allows aces to be set high and a trump suit
-        /// to be used.
-        /// </summary>
-        /// <param name="isAceHigh">If set to <c>true</c> is ace high.</param>
-        /// <param name="useTrumps">If set to <c>true</c> use trumps.</param>
-        /// <param name="trump">Trump.</param>
-        public Deck(bool isAceHigh, bool useTrumps, Suit trump) : this()
-        {
-            Card.isAceHigh = isAceHigh;
-            Card.useTrumps = useTrumps;
-            Card.trump = trump;
-        }
+        ///// <summary>
+        ///// Nondefault constructor. Allows aces to be set high and a trump suit
+        ///// to be used.
+        ///// </summary>
+        ///// <param name="isAceHigh">If set to <c>true</c> is ace high.</param>
+        ///// <param name="useTrumps">If set to <c>true</c> use trumps.</param>
+        ///// <param name="trump">Trump.</param>
+        //public Deck(bool isAceHigh, bool useTrumps, Suit trump) : this()
+        //{
+        //    Card.isAceHigh = isAceHigh;
+        //    Card.useTrumps = useTrumps;
+        //    Card.trump = trump;
+        //}
 
         public Card GetCard(int cardNum)
         {
             if (cardNum >= 0 && cardNum <= 51)
             {
                 if ((cardNum == 51) && (LastCardDrawn != null))
-                    LastCardDrawn(this, EventArgs.Empty);
+                    LastCardDrawn(this);
                 return cards[cardNum];
             }
             else
---
+++ Chapter13/Ch13CardLib/Ch13CardLib/PlayerState.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Ch13CardLib
+{
+    [Serializable]
+   public enum PlayerState
+    {
+        Inactive,
+        Active,
+        MustDiscard,
+        Winner,
+        Loser
+    }
+}
