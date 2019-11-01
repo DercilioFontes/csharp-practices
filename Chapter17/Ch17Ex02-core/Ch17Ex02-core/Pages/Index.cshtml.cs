59d6f7d22cb99191cafaaa2d7d8f7ffbbeff8cae
DercilioFontes <dercilioafontes@gmail.com>
2019-11-01 10:46:49 -0700

    Implemented the Ch17Ex02-core

---
+++ Chapter17/Ch17Ex02-core/Ch17Ex02-core/Pages/Index.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Ch17Ex02_core
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
