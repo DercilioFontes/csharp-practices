816788d583bc7503ffdf380e6bb9f5b2dc81fe4d
Dercilio Fontes <dercilio.fontes@snipp.com>
2019-12-04 10:08:30 -0800

    Added Chapter20 - MethodSyntax, OrderQueryResults and LargeNumberQuery

---
+++ Chapter20/LargeNumberQuery/LargeNumberQuery/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using static System.Console;
+
+namespace LargeNumberQuery
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] numbers = GenerateLotsOfNumbers(12_345_678);
+
+            var queryResults = from n in numbers
+                               where n < 1_000
+                               select n;
+
+            WriteLine("Numbers less than 1000:");
+
+            foreach(var item in queryResults)
+            {
+                WriteLine(item);
+            }
+
+            Write("Program finished, press Enter/Return to continue:");
+            _ = ReadLine();
+        }
+
+        private static int[] GenerateLotsOfNumbers(int count)
+        {
+            Random generator = new Random(0);
+            int[] result = new int[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                result[i] = generator.Next();
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Chapter20/OrderQueryResults/OrderQueryResults/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using static System.Console;
+
+namespace OrderQueryResults
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] names = { "Alonso", "Zheng", "Smith", "Jones", "Smythe", "Small", "Ruiz", "Hsieh", "Jorgenson", "Ilyich", "Singh", "Samba", "Fatimah" };
+
+            var queryResults = from n in names
+                               where n.StartsWith("S")
+                               orderby n/*.Substring(n.Length - 1)*/
+                               select n;
+
+            WriteLine("Names beginning with S ordered alphabetically:");
+
+            foreach(var item in queryResults)
+            {
+                WriteLine(item);
+            }
+
+            Write("Program finished, press Enter/Return to continue:");
+            _ = ReadLine();
+        }
+    }
+}
