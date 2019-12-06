f29874a63855766e43afcf1179edc5f93d178527
Dercilio Fontes <dercilio.fontes@snipp.com>
2019-12-06 15:31:09 -0800

    Added Chapter22 - Ch22Ex01

---
+++ Chapter22/Ch22Ex01/Ch22Ex01Client/Program.cs
@@ -0,0 +1,29 @@
+﻿using Ch22Ex01Client.ServiceReference1;
+using static System.Console;
+
+namespace Ch22Ex01Client
+{
+    class Program
+    {
+        static void Main()
+        {
+            Title = "Ch22Ex01Client";
+
+            string numericInput;
+            int intParam;
+            do
+            {
+                WriteLine("Enter an integer and press enter to call the WCF service.");
+                numericInput = ReadLine();
+            } while (!int.TryParse(numericInput, out intParam));
+
+            Service1Client client = new Service1Client();
+            CompositeType composite = new CompositeType { BoolValue = true, StringValue = "Casa"};
+
+            WriteLine("GetData: " + client.GetData(intParam));
+            WriteLine("GetDataUsingDataContrct: " + client.GetDataUsingDataContract(composite).StringValue);
+            WriteLine("Press an key to exit.");
+            _ = ReadKey();
+        }
+    }
+}
